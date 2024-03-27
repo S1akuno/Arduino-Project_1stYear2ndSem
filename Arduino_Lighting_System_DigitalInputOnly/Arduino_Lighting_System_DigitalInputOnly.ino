dd4b2196a550393d7257c40a0c4126380906d2ef
Christopher Jonota <70148137+christopherjonota@users.noreply.github.com>
2024-03-27 14:06:22 +0800

    UPDITID

--- Arduino_Lighting_System_DigitalInputOnly/Arduino_Lighting_System_DigitalInputOnly.ino
+++ Arduino_Lighting_System_DigitalInputOnly/Arduino_Lighting_System_DigitalInputOnly.ino
@@ -1,36 +1,33 @@
-///////////////////////  LIGHTING CONTROL SYSTEM  ////////////////////////////
+///////////////////////  DEFINING LIGHTING CONTROL SYSTEM  ////////////////////////////
 
-#define blackButton 2
-#define whiteButton 3
-#define yellowButton 4
-#define blueButton 5
-#define redButton 6
+  #define blackButton 2
+  #define whiteButton 3
+  #define yellowButton 4
+  #define blueButton 5
+  #define redButton 6
 
-#define ledWhite 7
-#define ledYellow 8
-#define ledBlue 9
-#define ledRed 10
+  #define ledWhite 7
+  #define ledYellow 8
+  #define ledBlue 9
+  #define ledRed 10
 
-boolean blue = false, red = false, yellow = false, white = false, totalOff = true, totalOn = false;
-// false means OFF
+  boolean blue = false, red = false, yellow = false, white = false, totalOff = true, totalOn = false;
+  // false means OFF
 
-///////////////////////  EARTHQUAKE ALARM SYSTEM   ////////////////////////////
-#define buzzer1 22
-#define ledBlue1 24
-#define blueButton1 26
-#define ledRed1 28
-#define redButton1 30
-#define buzzer2 32
+///////////////////////  DEFINING EARTHQUAKE ALARM SYSTEM   ////////////////////////////
+  #define buzzer1 22
+  #define ledBlue1 24
+  #define blueButton1 26
+  #define ledRed1 28
+  #define redButton1 30
+  #define buzzer2 32
 
-boolean redIndicator = false, blueIndicator = false;
+  boolean redIndicator = false, blueIndicator = false;
 
-/*///////////////////////  FIRE DETECTION ALARM SYSTEM  ////////////////////////////
 
-#define buzzer3 3
-int flameSensor1 = 2;
-*/
 void setup() {
-  ///////////////////  LIGHTING SYSTEM  ////////////////////////////////
+
+///////////////////  LIGHTING CONTROL SYSTEM SETUP////////////////////////////////
   pinMode(ledBlue, OUTPUT);
   pinMode(ledRed, OUTPUT);
   pinMode(ledYellow, OUTPUT);
@@ -42,7 +39,7 @@ void setup() {
   pinMode(blackButton, INPUT);
 
 
-///////////////////////  EARTHQUAKE ALARM SYSTEM   ////////////////////////////
+///////////////////////  EARTHQUAKE ALARM SYSTEM SETUP ////////////////////////////
   pinMode(buzzer1, OUTPUT);
   pinMode(buzzer2, OUTPUT);
   pinMode(ledBlue1, OUTPUT);
@@ -63,8 +60,95 @@ void setup() {
 }
 
 void loop() {
-////////////////////////  LIGHTING SYSTEM  ////////////////////////////////////////////
 
+///////////////////////  ARDUINO GUI  ////////////////////////////
+  if (Serial.available()){
+  String data = Serial.readString();
+  ///////////////////  LIGHTING SYSTEM  ////////////////////////////////
+    if(data == "redLED"){
+      if(red == false){
+        red = true;
+        Serial.println("Red LED turned ON");
+      }
+      else {
+        red = false;
+        Serial.println("Red LED turned OFF");
+      }
+    }
+    else if (data == "blueLED"){
+      if(blue == false){
+        blue = true;
+        Serial.println("Blue LED turned ON");
+      }
+      else if (blue == true) {  
+        blue = false;
+        Serial.println("Blue LED turned OFF");
+      }
+    }
+    else if (data == "yellowLED"){
+      if(yellow == false){
+        yellow = true;
+        Serial.println("Yellow LED turned ON");
+      }
+      else {
+        yellow = false;
+        Serial.println("Yellow LED turned OFF");
+      }
+    }
+    else if (data == "whiteLED"){
+      if(white == false){
+        white = true;
+        Serial.println("White LED turned ON");
+      }
+      else {
+        
+        white = false;
+        Serial.println("White LED turned OFF");
+      }
+    }
+    else if (data == "onAllLED"){
+        blue = true;
+        red = true;
+        yellow = true;
+        white = true;
+        totalOff = false;     // marks that the LEDs are turned ON
+        Serial.println("ALL LEDs turned ON");
+    }
+    else if (data == "offAllLED"){
+        blue = false;
+        red = false;
+        yellow = false;
+        white = false;
+        totalOff = true;          // marks that the LED's are off
+        Serial.println("ALL LEDs turned OFF");
+    }
+
+  ///////////////////////////  EARTHQUAKE  ///////////////////////
+    if(data == "Emergency"){
+      if(redIndicator == false){      // If the red button is clicked and the indicators are off, this will turn the alarm ON
+        redIndicator = true;
+        blueIndicator = false;    // turns off the other alarm if it is turned on
+        Serial.println("EARHTQUAKE ALARM ACTIVATED!!");
+      }
+      else if (redIndicator == true){ // If the red button is clicked and the indicators are ON, this will turn the alarm OFF
+        redIndicator = false;
+        Serial.println("EARHTQUAKE ALARM DEACTIVATED!!");
+      }  
+    }
+    if(data == "Drill"){
+      if(blueIndicator == false){
+        redIndicator = false;
+        blueIndicator = true;
+        Serial.println("EARHTQUAKE DRILL ALARM ACTIVATED!!");
+      }
+      else if (blueIndicator == true){
+        blueIndicator = false;
+        Serial.println("EARHTQUAKE DRILL ALARM DEACTIVATED!!");
+      }
+    }
+  }
+
+////////////////////////  LIGHTING SYSTEM  ////////////////////////////////////////////
   //Validation if the LEDs are turned off
   if (red == false || blue == false || yellow == false || white == false){
     totalOff = true;
@@ -74,72 +158,73 @@ void loop() {
   }
   // Black button Function
   if(digitalRead(blackButton) == HIGH){
-    if(totalOff == true){    //If the LEDs are all turned off, it will turn the LEDs ON
-      blue = true;
-      red = true;
-      yellow = true;
-      white = true;
-      totalOff = false;     // marks that the LEDs are turned ON
-      Serial.println("ALL LEDs turned ON");
+  if(totalOff == true){    //If the LEDs are all turned off, it will turn the LEDs ON
+    blue = true;
+    red = true;
+    yellow = true;
+    white = true;
+    totalOff = false;     // marks that the LEDs are turned ON
+    Serial.println("ALL LEDs turned ON");
 
-    }
-    else if (totalOff == false){ // If the LED'S are all turned ON, it will turn the LEDs OFF
-      blue = false;
-      red = false;
-      yellow = false;
-      white = false;
-      totalOff = true;          // marks that the LED's are off
-      Serial.println("ALL LEDs turned OFF");
+  }
+  else if (totalOff == false){ // If the LED'S are all turned ON, it will turn the LEDs OFF
+    blue = false;
+    red = false;
+    yellow = false;
+    white = false;
+    totalOff = true;          // marks that the LED's are off
+    Serial.println("ALL LEDs turned OFF");
 
-    }
+  }
   }
   // White  Button
   else if (digitalRead(whiteButton) == HIGH){
-    if(white == false){
-      white = true;
-      Serial.println("White LED turned ON");
-    }
-    else {
-      
-      white = false;
-      Serial.println("White LED turned OFF");
-    }
+  if(white == false){
+    white = true;
+    Serial.println("White LED turned ON");
+  }
+  else {
+    
+    white = false;
+    Serial.println("White LED turned OFF");
+  }
   }
   // Red Button
   else if (digitalRead(redButton) == HIGH){
-    if(red == false){
-      red = true;
-      Serial.println("Red LED turned ON");
-    }
-    else {
-      
-      red = false;
-      Serial.println("Red LED turned OFF");
-    }
+  if(red == false){
+    red = true;
+    Serial.println("Red LED turned ON");
+  }
+  else {
+    
+    red = false;
+    Serial.println("Red LED turned OFF");
+  }
   }
   // Blue Button
   else if (digitalRead(blueButton) == HIGH){
-    if(blue == false){
-      blue = true;
-      Serial.println("Blue LED turned ON");
-    }
-    else if (blue == true) {  
-      blue = false;
-      Serial.println("Blue LED turned OFF");
-    }
+  if(blue == false){
+    blue = true;
+    Serial.println("Blue LED turned ON");
+  }
+  else if (blue == true) {  
+    blue = false;
+    Serial.println("Blue LED turned OFF");
+  }
   }
   // Yellow Button
   else if (digitalRead(yellowButton) == HIGH){
-    if(yellow == false){
-      yellow = true;
-      Serial.println("Yellow LED turned ON");
-    }
-    else {
-      yellow = false;
-      Serial.println("Yellow LED turned OFF");
-    }
+  if(yellow == false){
+    yellow = true;
+    Serial.println("Yellow LED turned ON");
   }
-  ////////////////////////  CONDITIONALS  ///////////////////////////////////
+  else {
+    yellow = false;
+    Serial.println("Yellow LED turned OFF");
+  }
+  }
+
+//////////////////////// LIGHTING CONTROL SYSTEM CONTROLLER  ///////////////////////////////////
   if (red == true){
     digitalWrite(ledRed, HIGH);
   }
@@ -167,6 +252,7 @@ void loop() {
 
 
 ///////////////////////  EARTHQUAKE SYSTEM DECLARATION  ////////////////////////////
+
   if (digitalRead(redButton1) == HIGH) {  
     if(redIndicator == false){      // If the red button is clicked and the indicators are off, this will turn the alarm ON
       redIndicator = true;
@@ -190,6 +276,7 @@ void loop() {
     }
   } 
 
+//////////////////////// EARTHQUAKER ALARM SYSTEM CONTROLLER  ///////////////////////////////////
   if(redIndicator == true){
     digitalWrite(ledRed1, HIGH);
     tone(buzzer2, 1000);
@@ -201,7 +288,6 @@ void loop() {
     digitalWrite(ledRed1, LOW);
     noTone(buzzer2);
   }
-
   if(blueIndicator == true){
     digitalWrite(ledBlue1, HIGH);
     tone(buzzer1, 1000);
@@ -217,39 +303,6 @@ void loop() {
     digitalWrite(ledBlue1, LOW);
     noTone(buzzer1);
   }
-
-  ///////////////////////  FIRE DETECTION ALARM SYSTEM  ////////////////////////////
- /* int smokeSensorValue = analogRead(A0);
-  if (smokeSensorValue >= 400){
-    Serial.print("Sensor Value:");
-    Serial.print(smokeSensorValue);
-    Serial.print(" | Smoke Detected !!!");
-    Serial.println("");
-
-    digitalWrite(buzzer3, HIGH);
-    delay(1000);
-  }
-  else{
-    Serial.print("Smoke Sensor Value: ");
-    Serial.print(smokeSensorValue);
-    Serial.println("");
-    digitalWrite(buzzer3, LOW);
-
-  }
-    int flame = analogRead(A2);
-  if (digitalRead(flameSensor1) == HIGH){
-    Serial.println("SUNOOOOOGGGGG!!!");
-  }
-  if(digitalRead(flameSensor1) == LOW){
-    Serial.println("WALA SUNOG!!");
-    delay(2000);
-  }
-  Serial.println(flame);
-  */
-
-  
-  
-
 delay(200);
 }
 
